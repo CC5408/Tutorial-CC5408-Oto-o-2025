d19561230f6d3f7386cf01e1a4719b01ede48b60
Elías Zelada <elixs.777@gmail.com>
2025-03-28 12:57:55 -0300

    small fix

--- components/flip_sprite.gd
+++ components/flip_sprite.gd
@@ -1,7 +1,6 @@
 class_name FlipSprite
 extends Sprite2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _process(delta: float) -> void:
 	var direction = owner.transform.x.dot(Vector2.RIGHT)
